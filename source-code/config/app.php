41bbb7b79323fbf205442c7474ae08241b354247
frengki sinaga <fans.del04@gmail.com>
2024-09-17 11:14:54 +0700

    update url di app

--- source-code/config/app.php
+++ source-code/config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost:8081/source-code/public'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
